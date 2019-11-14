78bdbb0086266ffc945e83360f18b02fa53183f0
Ваня <ivanbeshka2002@gmail.com>
2019-11-14 21:39:35 +0500

    item post added

--- app/src/main/java/com/example/orthodoxapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/orthodoxapp/ui/home/HomeFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.orthodoxapp.R;
 
@@ -18,18 +19,15 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        final RecyclerView recyclerViewHome = root.findViewById(R.id.recyclerViewHome);
+
         return root;
     }
 }
